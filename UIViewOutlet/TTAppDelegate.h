613a530ea96f9a5ad7e08c87c741570f33bcefa0
sreshetnyak <reshetniak.sergey@gmail.com>
2014-02-13 10:32:34 +0200

    Initial commit from local git

---
+++ UIViewOutlet/TTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTAppDelegate.h
+//  UIViewOutlet
+//
+//  Created by sergey on 2/13/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
